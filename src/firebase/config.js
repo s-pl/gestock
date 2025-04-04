d1df89cb896c052ec2455f7996ba3d35364aedae
Skitt <samuelponceluna@gmail.com>
2025-04-04 11:03:31 +0100

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+
+  };
+  
+
+  
+
+
+const app = initializeApp(firebaseConfig);
+
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
+export default app;
